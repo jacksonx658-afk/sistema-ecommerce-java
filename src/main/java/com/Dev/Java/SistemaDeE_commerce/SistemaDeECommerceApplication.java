ea2af66496050befc9fec6999f8204e8cfe3020e
jacksonx658-afk <seu_email_do_github@example.com>
2026-09-15 16:04:42 -0300

    feat: estrutura inicial do projeto e-commerce com spring boot

---
+++ src/main/java/com/Dev/Java/SistemaDeE_commerce/SistemaDeECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.Dev.Java.SistemaDeE_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dev/Java/SistemaDeE_commerce/SistemaDeECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dev.Java.SistemaDeE_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
