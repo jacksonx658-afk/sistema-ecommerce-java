bedd8e3a2afa8540bd8c1f8abea49e8199239a4c
jacksonx658-afk <seu_email_do_github@example.com>
2026-09-15 16:33:26 -0300

    feat: Criar a entidade ProdutoModelo com anotações JPA e validações

---
+++ src/main/java/com/Dev/Java/SistemaDeE_commerce/Produto/ProdutoModelo.java
@@ -0,0 +1,35 @@
+package com.Dev.Java.SistemaDeE_commerce.Produto;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "tb_produtos")
+
+public class ProdutoModelo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome do produto é obrigatório")
+    private String nome;
+
+    private String descricao;
+
+    @NotNull(message = "O preço é obrigatório")
+    @DecimalMin(value = "0.01", message = "O preço deve ser maior que zero")
+    private BigDecimal preco;
+
+    @NotNull(message = "A quantidade em estoque é obrigatória")
+    @Min(value = 0, message = "O estoque não pode ser negativo")
+    private Integer quantidadeEstoque;
+
+    }
